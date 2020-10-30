18b57be524d1216354e5426e94c3b17dda9765cf
Saurabh <saurabh.d2106@gmail.com>
2020-10-30 07:22:34 +0000

    Extent Report code added

--- ModularFramework/src/test/java/in/amazon/test/BaseTest.java
+++ ModularFramework/src/test/java/in/amazon/test/BaseTest.java
@@ -2,9 +2,15 @@ package in.amazon.test;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.Status;
 
 import commonLibs.implementation.CommonDriver;
+import commonLibs.utils.DateUtils;
+import commonLibs.utils.ExtentReportUtils;
 import in.amazon.pages.Homepage;
 
 public class BaseTest {
@@ -17,8 +23,40 @@ public class BaseTest {
 
 	WebDriver driver;
 
+	ExtentReportUtils extentReportUtils;
+
+	static String htmlReportFilename;
+	static String currentWorkingDirectory;
+	static String executeStartTime;
+
+	static {
+		executeStartTime = DateUtils.getCurrentDate();
+
+		currentWorkingDirectory = System.getProperty("user.dir");
+
+		htmlReportFilename = String.format("%s/reports/amazon-report-%s.html", currentWorkingDirectory,
+				executeStartTime);
+		
+		System.out.println(htmlReportFilename);
+
+	}
+
+	@BeforeSuite
+	public void preSetup() {
+		initializeReports();
+	}
+
+	@AfterSuite
+	public void postCLeanup() {
+
+		flushReports();
+
+	}
+
 	@BeforeClass
 	public void setup() throws Exception {
+
+		extentReportUtils.createATestcase("Setup - All setup for test execution");
 
 		invokeBrowser();
 
@@ -38,12 +76,22 @@ public class BaseTest {
 	}
 
 	private void invokeBrowser() throws Exception {
-		cmnDriver = new CommonDriver("chrome");
+		String browserType = "chrome";
 
-		cmnDriver.setPageloadTimeout(90);
+		cmnDriver = new CommonDriver(browserType);
 
-		cmnDriver.setElementDetectionTimeout(20);
+		extentReportUtils.addLogStatus(Status.INFO, "Browser invoked is - " + browserType);
 
+		int pageloatimeout = 90;
+		cmnDriver.setPageloadTimeout(pageloatimeout);
+		
+		extentReportUtils.addLogStatus(Status.INFO, "Pageload timeout - " + pageloatimeout);
+
+		int elementDetectionTimeout = 30;
+		cmnDriver.setElementDetectionTimeout(elementDetectionTimeout);
+
+		extentReportUtils.addLogStatus(Status.INFO, "Implict timeout - " + elementDetectionTimeout);
+		
 		cmnDriver.navigateToUrl(url);
 
 		driver = cmnDriver.getDriver();
@@ -52,6 +100,17 @@ public class BaseTest {
 
 	private void pageInitialization() {
 		homepage = new Homepage(driver);
+	}
+
+	private void initializeReports() {
+
+		extentReportUtils = new ExtentReportUtils(htmlReportFilename);
+	}
+
+	private void flushReports() {
+
+		extentReportUtils.flushReport();
+
 	}
 
 }
--- ModularFramework/src/test/java/in/amazon/test/HomepageTests.java
+++ ModularFramework/src/test/java/in/amazon/test/HomepageTests.java
@@ -1,17 +1,28 @@
 package in.amazon.test;
 
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
 import org.testng.annotations.Test;
 
 public class HomepageTests extends BaseTest {
 
 	@Test
 	public void verifySearchProduct() throws Exception {
+		extentReportUtils.createATestcase("TC001 - Verify Search Product");
+
 		String product = "Apple Watch";
 
 		String category = "Electronics";
 
+		extentReportUtils.addLogStatus(Status.INFO, "Product searched - " + product);
+
+		extentReportUtils.addLogStatus(Status.INFO, "Category selected - " + category);
+
 		String result = homepage.searchProduct(product, category);
+
+		extentReportUtils.addLogStatus(Status.INFO, "Results - " + result);
 
 		System.out.println(result);
 
